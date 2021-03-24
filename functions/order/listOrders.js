3e4cb2edf17246ad2abbbe06433da23c7d5f061c
Clara Dias <claradprado@gmail.com>
2021-03-24 14:50:06 -0300

    Add novas funçoes

---
+++ functions/order/listOrders.js
@@ -0,0 +1,29 @@
+import handler from "../../libs/handler-lib";
+import dynamoDb from "../../libs/dynamodb-lib";
+const table=process.env.tableName;
+
+export const main = handler(async (event, context) => {
+  const params = {
+    TableName: table,
+    // 'KeyConditionExpression' defines the condition for the query
+    // - 'userId = :userId': only return items with matching 'userId'
+    //   partition key
+    KeyConditionExpression: "#user= :username",
+    ExpressionAttributeNames:{
+        "#user": "username"
+    },
+    // 'ExpressionAttributeValues' defines the value in the condition
+    // - ':userId': defines 'userId' to be the id of the author
+    ExpressionAttributeValues: {
+      ":username": event.requestContext.identity.cognitoIdentityId,
+    },
+  };
+
+  const result = await dynamoDb.query(params);
+   if ( ! result.Items) {
+        throw new Error("Não há produtos.");
+      }
+
+  // Return the matching list of items in response body
+  return result.Items;
+});
---
+++ functions/order/sendOrderEmail.js
@@ -0,0 +1,33 @@
+import handler from "../../libs/handler-lib";
+import nodemailer from 'nodemailer';
+
+//funcao para enviar email apos a compra
+export const main = handler(async (event, context) => {
+    const sucessSend = {
+        message: ''
+    };
+    //adiciona as informações de email e senha
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      port: '587',
+      auth: {
+          user: 'devclara.bgc@gmail.com',
+          pass: 'gugxcbdaspzusurm'
+      }
+    });
+    // envia o email utilizndo o objeto transporter criado
+    let info = await transporter.sendMail({
+    from: '"Minion Shop" <devclara.bgc@gmail.com>',
+    to: "claradprado.aws@gmail.com", // lista de envio
+    subject: "Nova reserva realizada!", // Assunto
+    text: "Nova reserva feita no Minion Shop!",
+    html: "<h1>Nova reserva!</h1><h4>Seu minion está a caminho</h4>", // html body
+  });
+    if (!info.messageId) {
+        sucessSend.message = 'Não conseguimos fazer a reserva!';
+    } else {
+        sucessSend.message = 'Reservado com sucesso!';
+    }
+    return sucessSend.message;
+});
+
